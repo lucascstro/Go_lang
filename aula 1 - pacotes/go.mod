482bee2c9b7af709f13b41377a07c7f69d158c6b
lucas.castro <lucas.castro@cxcontact.com.br>
2025-06-16 00:21:39 -0300

    importando modulos de terceiros

--- aula 1 - pacotes/go.mod
+++ aula 1 - pacotes/go.mod
@@ -1,3 +1,9 @@
 module modulo
 
 go 1.24.4
+
+require github.com/badoux/checkmail v1.2.4 // indirect
+
+
+//para importar um modulo de terceito usar go get github.com/badoux/checkmail
+//para limpar referencias não usadas usar go mod tiddy
--- aula 1 - pacotes/main.go
+++ aula 1 - pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("teste@teste.com")
+	fmt.Println(erro)
 }
